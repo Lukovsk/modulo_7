a5ab543df2929ad7d486e0404715103ed6b5fc29
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-08-27 18:06:35 -0300

    inicio de um backend em java

---
+++ ponderadas/ponderada_2/backend/myTasks/src/test/java/com/example/myTasks/MyTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
